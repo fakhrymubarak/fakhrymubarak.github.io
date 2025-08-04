d5724a39d497a895b58312f7f089f09fe68c5dd1
Fakhry <fakhrymubarak@gmail.com>
2025-08-04 14:44:22 +0700

    fix: pipeline issues

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
