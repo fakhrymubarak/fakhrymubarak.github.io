9fbdd263f6df63b84734c0e5702c4c03c62d804c
Fakhry <fakhrymubarak@gmail.com>
2025-08-16 14:33:31 +0700

    add: vite configuration

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCSR2TiaMwWx-NY9BVpHM2PbxannEBzlNU',
-  authDomain: 'fakhry-showcase-v2.firebaseapp.com',
-  projectId: 'fakhry-showcase-v2',
-  storageBucket: 'fakhry-showcase-v2.firebasestorage.app',
-  messagingSenderId: '686700373210',
-  appId: '1:686700373210:web:5b9c63f4e214511ae171e3',
-  measurementId: 'G-29VSW7CMFR',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
