3bc8acfd74f68b2f843f7b85293346bcc6ce4d38
fakhrymubarak <fakhrymubarak@users.noreply.github.com>
2025-10-03 11:31:39 +0000

    deploy: 6238b9fbdb88780cb5946dd022a02b931ed04b34

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for cache management and automatic updates
 // This is a template - the build script will inject the actual version
-const APP_VERSION = 'release/v1.1.1'; // Will be replaced by a build script
+const APP_VERSION = 'release/v.1.2.0'; // Will be replaced by a build script
 const CACHE_NAME = `showcase-${APP_VERSION}-cache`;
 const STATIC_CACHE_NAME = `showcase-${APP_VERSION}-static`;
 
